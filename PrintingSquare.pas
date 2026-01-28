dfc212273f6c5715b8a7f211e8c6e6581a3f9180
Alexander <pchp250g8@tutamail.com>
2026-01-28 12:11:35 +0300

    Created Project PrintingSquare

---
+++ PrintingSquare.pas
@@ -0,0 +1,17 @@
+﻿uses
+  System,
+  System.Threading;
+begin
+  var strLine := 'Hello,World!!! Printing Square!!!';
+  Console.Clear();
+  Console.CursorVisible := False;
+  for var i : Integer := 0 to strLine.Length - 1 do
+  begin
+    Console.SetCursorPosition(10 + i, 10);
+    Console.Write(strLine[i + 1] + chr($2588));
+    Thread.Sleep(500);
+  end;
+  Console.SetCursorPosition(10, 10);
+  Console.Write(strLine + ' ');
+  Console.ReadKey();
+end.
